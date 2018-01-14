b729cd00c61d544b94a9b3f846b3d6b05478e210
Symphorien Gibol <symphorien+git@xlumurb.eu>
2018-01-15 00:15:50 +0100

    Remove hard coded paths from build script

--- libstore/build.rs
+++ libstore/build.rs
@@ -1,5 +1,6 @@
 extern crate bindgen;
 extern crate cc;
+extern crate shlex;
 
 use std::env;
 use std::path::PathBuf;
@@ -8,20 +9,8 @@ fn main() {
     println!("cargo:rustc-link-lib=nixstore");
     println!("cargo:rustc-link-lib=nixmain");
 
-    let bindings = bindgen::Builder::default()
-        .clang_args(vec!(
-"-std=c++11",
-"-idirafter"
-,"/nix/store/mqalq0v2laqblw00dp7pwwckj2ra6jyh-glibc-2.26-75-dev/include"
-,"-isystem"
-,"/nix/store/c30dlkmiyrjxxjv6nv63igjkzcj1fzxi-gcc-6.4.0/include/c++/6.4.0"
-,"-isystem"
-,"/nix/store/c30dlkmiyrjxxjv6nv63igjkzcj1fzxi-gcc-6.4.0/include/c++/6.4.0/x86_64-unknown-linux-gnu"
-, "-isystem"
-,"/nix/store/lzv2dd0wrjf8d3c4nlfhcgl668jwvdri-boost-1.65.1-dev/include"
-, "-isystem"
-,"/nix/store/cqhdk51xqxj1990v20y3wfnvhr0r8yds-nix-1.11.15-dev/include"
-  ))
+    let mut builder = bindgen::Builder::default()
+        .clang_arg("-std=c++11")
         .header("wrapper.hpp")
         .whitelist_type("nix::LocalStore")
         .whitelist_type("nix::RemoteStore")
@@ -32,9 +21,14 @@ fn main() {
         .whitelist_function("nix::openStore")
         .whitelist_function("nix_adapter::.*")
         .opaque_type("std::.*")
-        .impl_debug(true)
-        .generate()
-        .expect("Unable to generate bindings");
+        .impl_debug(true);
+
+    if let Ok(cflags) = env::var("BINDGEN_EXTRA_CFLAGS") {
+        let extra_args = shlex::split(&cflags).expect("Cannot parse $BINDGEN_EXTRA_CFLAGS");
+        builder = builder.clang_args(extra_args);
+    }
+
+    let bindings = builder.generate().expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
--- shell.nix
+++ shell.nix
@@ -1,14 +1,37 @@
 with import <nixpkgs> {}; 
-stdenv.mkDerivation {
-  name = "nix-du-build-env";
-  buildInputs = [
-    llvm
-    cargo
+let buildInputs = [
     boost
     nix
+  ]; in
+/*
+nix-du is built with bindgen, which in turn uses the (unwrapped) libclang.so.
+It allows to pass the (missing) flags to include the libc and lib(std)cxx as
+an environment variable.
+This derivation is a file containing these flags.
+
+We use clang because with gcc the path to libstdcxx is not found...
+*/
+let bindgenFlags = clangStdenv.mkDerivation {
+  name = "nix-du-bindgen-flags";
+  inherit buildInputs;
+  # FIXME: fragile...
+  buildCommand = ''
+  NIX_DEBUG=1 clang++ -x c++ /dev/null -c -o /dev/null  |& awk 'BEGIN { print_next=0; } { if (print_next) { print $0; print_next=0; } } /-i/ { print_next = 1; print $0; }' > $out
+  '';
+}; in
+stdenv.mkDerivation rec {
+  name = "nix-du-build-env";
+  inherit buildInputs;
+  nativeBuildInputs = [
+    llvm
+    cargo
   ];
   CLANG_PATH="${clang}/bin/clang";
   LIBCLANG_PATH="${llvmPackages.clang-unwrapped}/lib";
   LD_LIBRARY_PATH="${llvmPackages.clang-unwrapped}/lib";
   RUST_BACKTRACE=1;
+  preConfigure = ''
+    export BINDGEN_EXTRA_CFLAGS="$(cat ${bindgenFlags})";
+  '';
+  shellHook = preConfigure;
 }
