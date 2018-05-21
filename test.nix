139adff819b5bcf1c84931223c749801c0d04bf8
Symphorien Gibol <symphorien+git@xlumurb.eu>
2018-05-21 21:35:10 +0200

    test.nix: build on nixos-18.03 and nixpkgs-unstable

--- test.nix
+++ test.nix
@@ -1,2 +1,3 @@
-with import <nixpkgs> {};
-map (x: callPackage ./nix-du.nix { nix = x; }) [ nixStable nixUnstable ]
+map (url: with import (builtins.fetchTarball url) {};
+  map (x: callPackage ./nix-du.nix { nix = x; }) [ nixStable nixUnstable ]
+) [ channel:nixos-18.03 channel:nixpkgs-unstable ]
