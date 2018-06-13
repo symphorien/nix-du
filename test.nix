56242b3d8b01923849c959c9b3ad3d3ad561ff9f
Symphorien Gibol <symphorien+git@xlumurb.eu>
2018-06-13 11:26:47 +0200

    run tests on patched nix (see nix PR #2223)
    this PR fixes spurious test failures

--- test.nix
+++ test.nix
@@ -1,3 +1,13 @@
 map (url: with import (builtins.fetchTarball url) {};
-  map (x: callPackage ./nix-du.nix { nix = x; }) [ nixStable nixUnstable ]
+# this function patches nix to fix an issue causing spurious test failures
+# FIXME: remove when the channel contains the patch
+let fix2223 = nix: nix.overrideAttrs(old: {
+  doInstallCheck = false;
+  patches = [ (fetchpatch {
+    url = https://patch-diff.githubusercontent.com/raw/NixOS/nix/pull/2223.patch;
+    sha256 = "0ykmqas4qsqj3xhb26x8i9711k5sb0x84g779ms3zp083ckpdbhf";
+  }) ];
+});
+in
+  map (x: callPackage ./nix-du.nix { nix = fix2223 x; }) [ nixStable nixUnstable ]
 ) [ channel:nixos-18.03 channel:nixpkgs-unstable ]
