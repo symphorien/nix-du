4d59d8105a7526c25c4b1ce383741384674c5f8b
Symphorien Gibol <symphorien+git@xlumurb.eu>
2018-06-17 10:16:26 +0200

    further nix patch

--- test.nix
+++ test.nix
@@ -3,10 +3,16 @@ map (url: with import (builtins.fetchTarball url) {};
 # FIXME: remove when the channel contains the patch
 let fix2223 = nix: nix.overrideAttrs(old: {
   doInstallCheck = false;
-  patches = [ (fetchpatch {
+  patches = [
+    (fetchpatch {
     url = https://patch-diff.githubusercontent.com/raw/NixOS/nix/pull/2223.patch;
     sha256 = "0ykmqas4qsqj3xhb26x8i9711k5sb0x84g779ms3zp083ckpdbhf";
-  }) ];
+    })
+    (fetchpatch {
+    url = https://patch-diff.githubusercontent.com/raw/NixOS/nix/pull/2234.patch;
+    sha256 = "0pwwx4833f16082rddp2jbvcra3avnggci1c0v7fr72glsvm6vrb";
+    })
+  ];
 });
 in
   map (x: callPackage ./nix-du.nix { nix = fix2223 x; }) [ nixStable nixUnstable ]
