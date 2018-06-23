92870071a5e831f9b362caee4198182492d266ae
Symphorien Gibol <symphorien+git@xlumurb.eu>
2018-06-23 17:05:17 +0200

    also fix nix when building default.nix

---
+++ fix.nix
@@ -0,0 +1,16 @@
+{nix, fetchpatch}:
+# this function patches nix to fix an issue causing spurious test failures
+# FIXME: remove when the channel contains the patch
+nix.overrideAttrs(old: {
+  doInstallCheck = false;
+  patches = [
+    (fetchpatch {
+    url = https://patch-diff.githubusercontent.com/raw/NixOS/nix/pull/2223.patch;
+    sha256 = "0ykmqas4qsqj3xhb26x8i9711k5sb0x84g779ms3zp083ckpdbhf";
+    })
+    (fetchpatch {
+    url = https://patch-diff.githubusercontent.com/raw/NixOS/nix/pull/2234.patch;
+    sha256 = "0pwwx4833f16082rddp2jbvcra3avnggci1c0v7fr72glsvm6vrb";
+    })
+  ];
+})
--- test.nix
+++ test.nix
@@ -1,19 +1,6 @@
 map (url: with import (builtins.fetchTarball url) {};
 # this function patches nix to fix an issue causing spurious test failures
 # FIXME: remove when the channel contains the patch
-let fix2223 = nix: nix.overrideAttrs(old: {
-  doInstallCheck = false;
-  patches = [
-    (fetchpatch {
-    url = https://patch-diff.githubusercontent.com/raw/NixOS/nix/pull/2223.patch;
-    sha256 = "0ykmqas4qsqj3xhb26x8i9711k5sb0x84g779ms3zp083ckpdbhf";
-    })
-    (fetchpatch {
-    url = https://patch-diff.githubusercontent.com/raw/NixOS/nix/pull/2234.patch;
-    sha256 = "0pwwx4833f16082rddp2jbvcra3avnggci1c0v7fr72glsvm6vrb";
-    })
-  ];
-});
-in
+let fix2223 = nix: callPackage ./fix.nix { inherit nix; }; in
   map (x: callPackage ./nix-du.nix { nix = fix2223 x; }) [ nixStable nixUnstable ]
 ) [ channel:nixos-18.03 channel:nixpkgs-unstable ]
