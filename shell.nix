1c54ee635e6bc0f879b42d5d538715dd15438e46
Symphorien Gibol <symphorien+git@xlumurb.eu>
2019-07-13 14:21:38 +0200

    add rls and rustfmt to nix-shell

--- shell.nix
+++ shell.nix
@@ -1,3 +1,3 @@
 ((import ./default.nix).override(_: {source = null;})).overrideAttrs(old: {
-  nativeBuildInputs = (old.nativeBuildInputs or []) ++ (with (import <nixpkgs> {}); [ rust-bindgen ]);
+  nativeBuildInputs = (old.nativeBuildInputs or []) ++ (with (import <nixpkgs> {}); [ rust-bindgen rls rustfmt ]);
 })
