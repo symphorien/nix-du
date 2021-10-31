b74de974732c1f26147367943825757cd3ec435c
Guillaume Girol <symphorien+git@xlumurb.eu>
2021-10-31 12:00:00 +0000

    actually test with nix unstable

--- test.nix
+++ test.nix
@@ -1,3 +1,3 @@
 map (url: with import (builtins.fetchTarball url) {};
-  map (x: callPackage ./nix-du.nix {}) [ nixStable nixUnstable ]
+  map (x: callPackage ./nix-du.nix { nix = x; }) [ nixStable nixUnstable ]
 ) [ channel:nixos-20.09 channel:nixpkgs-unstable ]
