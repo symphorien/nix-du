7d625c107e768746085ac927a38d93cbe2522fd9
Guillaume Girol <symphorien+git@xlumurb.eu>
2022-03-15 12:00:00 +0000

    test all nix versions

--- test.nix
+++ test.nix
@@ -1,4 +1,11 @@
+let
+  isDerivation = x: (x.type or null) == "derivation";
+  allNixVersions = pkgs: [ pkgs.nixStable pkgs.nixUnstable ] ++ (builtins.filter isDerivation (builtins.attrValues (pkgs.nixVersions or { })));
+in
 map
-  (url: with import (builtins.fetchTarball url) { };
-  map (x: (callPackage ./nix-du.nix { nix = x; }).tested) [ nixStable /* nixUnstable */ ]
+  (url:
+    let
+      pkgs = import (builtins.fetchTarball url) { };
+    in
+    map (x: (pkgs.callPackage ./nix-du.nix { nix = x; }).tested) (allNixVersions pkgs)
   ) [ /* channel:nixos-21.11 */ channel:nixpkgs-unstable ]
