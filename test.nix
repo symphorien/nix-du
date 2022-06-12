762ef4b3dbbd2e56bdce706873228be810fc2d6a
Guillaume Girol <symphorien+git@xlumurb.eu>
2022-06-12 12:00:00 +0000

    be a bit more specific about which test fail when they fail

--- test.nix
+++ test.nix
@@ -6,6 +6,7 @@ map
   (url:
     let
       pkgs = import (builtins.fetchTarball url) { };
+      channel_name = builtins.replaceStrings [ "channel:" ] [ "" ] (builtins.toString url);
     in
-    map (x: (pkgs.callPackage ./nix-du.nix { nix = x; }).tested) (allNixVersions pkgs)
+    map (x: (pkgs.callPackage ./nix-du.nix { nix = x; }).tested.overrideAttrs (old: { name = old.name + "-${channel_name}-nix-(${x.version}"; })) (allNixVersions pkgs)
   ) [ channel:nixos-22.05 channel:nixpkgs-unstable ]
